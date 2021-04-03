15564d317456a1c1596ded3f4c4b83c91a3d2f41
honeyetina <tina.sf2014@gmail.com>
2021-04-02 23:13:25 -0700

    python challeng

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,3 +5,35 @@ Created on Sat Mar 27 10:51:51 2021
 
 @author: tinatina
 """
+import pandas as pd
+budget_data = pd.read_csv('./Resources/budget_data.csv')
+n = budget_data['Date'].nunique()
+# print(n)
+m = budget_data['Profit/Losses'].sum()
+# print(m)
+budget_data['profit_shift'] = budget_data['Profit/Losses'].shift(1)
+budget_data['profit_change'] = budget_data['Profit/Losses'] - budget_data['profit_shift']
+profit_change_mean = budget_data['profit_change'].mean()
+# print(profit_change_mean)
+s = budget_data['Profit/Losses'].argmax()
+s1 = budget_data.loc[s,"Date"]
+# print(s1)
+s2 = budget_data.loc[s,"profit_change"]
+# print(s2)
+d = budget_data['Profit/Losses'].argmin()
+d1 = budget_data.loc[d,"Date"]
+d2 = budget_data.loc[d,"profit_change"]
+# print(d1)
+# print(d2)
+
+result = f'''Financial Analysis
+----------------------------
+Total Months: {n}
+Total: ${m}
+Average  Change: ${profit_change_mean:.2f}
+Greatest Increase in Profits: {s1} (${s2:.0f})
+Greatest Decrease in Profits: {d1} (${d2:.0f})'''
+
+print(result)
+with open('./analysis/' + "analysis.txt" , "w") as text_file:
+    text_file.write(result)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,3 +5,28 @@ Created on Fri Apr  2 16:41:21 2021
 
 @author: tinatina
 """
+import pandas as pd
+election_data = pd.read_csv('./Resources/election_data.csv')
+election_data.head()
+total_vote_cast = election_data["Voter ID"].count()
+print(total_vote_cast)
+cv = (election_data["Candidate"]).value_counts()
+print(cv)
+cv_pct = cv/total_vote_cast
+print(cv_pct)
+winner = cv.index[0]
+print(winner)
+result = f'''Election Results
+-------------------------
+Total Votes: {total_vote_cast}
+-------------------------
+{cv.index[0]}: {cv_pct.iloc[0]:.3%} ({cv.iloc[0]})
+{cv.index[1]}: {cv_pct.iloc[1]:.3%} ({cv.iloc[1]})
+{cv.index[2]}: {cv_pct.iloc[2]:.3%} ({cv.iloc[2]})
+{cv.index[3]}: {cv_pct.iloc[3]:.3%} ({cv.iloc[3]})
+-------------------------
+Winner: {winner}
+-------------------------'''
+print(result)
+with open('./analysis/' + "analysis.txt" , "w") as text_file:
+    text_file.write(result)
